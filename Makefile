8ba5ee90a55bb140287dec3824a4745ef72d92ca
Hanai <ihanai1991@gmail.com>
2019-05-12 15:52:06 +0800

    update

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ MKDIR_P = mkdir -p
 OUT_DIR = output
 SRC_DIR = source
 
+GLAD_FLAGS = -ldl
+
 .PHONY: directories
 
 all: directories build
@@ -11,8 +13,13 @@ directories: ${OUT_DIR}
 ${OUT_DIR}:
 	${MKDIR_P} ${OUT_DIR}
 
-build:
-	g++ ${SRC_DIR}/hello_triangle.cpp -o ${OUT_DIR}/hello_triangle -lGL -lglfw -lGLEW
+build: hello_window hello_triangle
+
+hello_window:
+	g++ ${SRC_DIR}/glad.c ${SRC_DIR}/hello_window.cpp -o ${OUT_DIR}/hello_window -I include $(GLAD_FLAGS) -lGLEW -lglfw -lGL -lX11 -lpthread -lXrandr -lXi
+
+hello_triangle:
+	g++ ${SRC_DIR}/hello_triangle.cpp -o ${OUT_DIR}/hello_triangle -I include $(GLAD_FLAGS) -lGLEW -lglfw -lGL -lX11 -lpthread -lXrandr -lXi
 
 clean:
 	rm -rf ${OUT_DIR}
--- source/hello_triangle.cpp
+++ source/hello_triangle.cpp
@@ -2,14 +2,17 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
-int main() {
-	if (!glfwInit()) {
+int main()
+{
+	if (!glfwInit())
+	{
 		fprintf(stderr, "ERROR: could not start GLFW3\n");
 		return 1;
 	}
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
-	if (!window) {
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+	if (!window)
+	{
 		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
 		glfwTerminate();
 		return 1;
@@ -19,8 +22,8 @@ int main() {
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	const GLubyte* renderer = renderer = glGetString(GL_RENDERER);
-	const GLubyte* version = glGetString(GL_VERSION);
+	const GLubyte *renderer = renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
 
 	printf("Renderer: %s\n", renderer);
 	printf("OpenGL version supported %s\n", version);
@@ -31,8 +34,7 @@ int main() {
 	float points[] = {
 		0.0f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f
-	};
+		-0.5f, -0.5f, 0.0f};
 
 	GLuint vbo = 0;
 	glGenBuffers(1, &vbo);
@@ -46,19 +48,19 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-	const char* vertex_shader =
-"#version 400\n"
-"in vec3 vp;"
-"void main() {"
-"  gl_Position = vec4(vp, 1.0);"
-"}";
+	const char *vertex_shader =
+		"#version 400\n"
+		"in vec3 vp;"
+		"void main() {"
+		"  gl_Position = vec4(vp, 1.0);"
+		"}";
 
-	const char* fragment_shader =
-"#version 400\n"
-"out vec4 frag_colour;"
-"void main() {"
-"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
-"}";
+	const char *fragment_shader =
+		"#version 400\n"
+		"out vec4 frag_colour;"
+		"void main() {"
+		"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+		"}";
 
 	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vs, 1, &vertex_shader, NULL);
@@ -72,7 +74,8 @@ int main() {
 	glAttachShader(shader_programme, vs);
 	glLinkProgram(shader_programme);
 
-	while(!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(shader_programme);
 		glBindVertexArray(vao);
